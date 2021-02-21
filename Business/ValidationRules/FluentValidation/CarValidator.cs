1a684db93a1c57f060479964b6e2a9c4e03658fd
eren aydinç <erenaydinc@hotmail.com>
2021-02-21 13:47:16 +0300

    car rental project autofac fluent validation aop

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entites.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.DailyPrice).GreaterThan(0).NotEmpty();
+            RuleFor(p => p.ModelYear).NotEmpty();
+            RuleFor(p => p.BrandId).NotEmpty();
+            RuleFor(p => p.Description).MinimumLength(2);
+            RuleFor(p => p.Description).NotEmpty();
+            RuleFor(p => p.ColorId).NotEmpty();
+        }
+    }
+}
